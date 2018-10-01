8bcd7ca19fa4fa91c0a918400c5c78708a5f39aa
Octowl <alsaff1987@gmail.com>
2018-10-01 11:16:42 +0300

    Reduce to skeleton

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -3,7 +3,3 @@ export const GET_ERRORS = "GET_ERRORS";
 
 // Auth Types
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
-
-// Intent Types
-export const GET_INTENTS = "GET_INTENTS";
-export const INTENTS_LOADING = "INTENTS_LOADING";
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,8 @@ import { combineReducers } from "redux";
 // Reducers
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
-import intentReducer from "./intentReducer";
 
 export default combineReducers({
   auth: authReducer,
-  errors: errorReducer,
-  intents: intentReducer
+  errors: errorReducer
 });
